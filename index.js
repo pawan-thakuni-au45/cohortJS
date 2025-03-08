26d73eea6dda50a6dd5a14d137c1982a78f004b3
pawan <thakunipawan@gmail.com>
2025-03-08 21:33:34 +0530

    comit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+// const fs=require("fs")
+// const read=(err,data)=>{
+//     console.log(data)
+// }
+// const a=fs.readFile("a.txt","utf-8",read)
+
+// console.log(a);
+// const b=fs.readFile("b.txt","utf-8",read)
+
+// console.log(b);
+// console.log("jji")
+
+console.log("hee") //will go in callstack in very frist
+
+function timeout(){
+    console.log('time');
+}
+setTimeout(timeout,5000)// this will go in callstack but it is taking time so it will be managed by "WEB API"{it is a async}
+
+console.log('done!'); //Tin second num this line will run ,and in the very last settimeout will run 
+
+first it will go in call stack ,then if it is async then it will go in web api ,and call stack will do its work ,then web api-->callstach queuq -->callback
